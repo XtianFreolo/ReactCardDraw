8a5d8658ef8e6887760e41e2f34439ac3a8ada69
XtianFreolo <F60@umbc.edu>
2025-06-14 21:30:25 -0400

    Card.jsx done

---
+++ src/assets/Card.jsx
@@ -0,0 +1,14 @@
+
+
+function Card({ name, image }) {
+
+    const [{ angle, xPos, yPos }] = useState({
+        angle: Math.random() * 90 - 45,
+        xPos: Math.random() * 40 - 20,
+        yPos: Math.random() * 40 - 20,
+    });
+
+    const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+
+    return <img className="Card" style={{ transform }} src={image} alt={name} />;
+}
